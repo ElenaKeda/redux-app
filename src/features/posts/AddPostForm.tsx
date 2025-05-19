f6260e9935f544eea400f323bbfef9b4ea748f2d
Elena Keda <elena_keda_92@mail.ru>
2025-05-19 17:12:12 +0300

    added addPost method

---
+++ src/features/posts/AddPostForm.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { nanoid } from '@reduxjs/toolkit'
+
+import { useAppDispatch } from '@/app/hooks'
+
+import { type Post, addPost } from './postsSlice'
+
+interface AddPostFormFields extends HTMLFormControlsCollection {
+  postTitle: HTMLInputElement
+  postContent: HTMLTextAreaElement
+}
+
+interface AddPostFormElements extends HTMLFormElement {
+  readonly elements: AddPostFormFields
+}
+
+export const AddPostForm = () => {
+  const dispatch = useAppDispatch()
+
+  const handleSubmit = (e: React.FormEvent<AddPostFormElements>) => {
+    e.preventDefault()
+
+    const { elements } = e.currentTarget
+    const title = elements.postTitle.value
+    const content = elements.postContent.value
+
+    const newPost: Post = {
+      id: nanoid(),
+      title,
+      content,
+    }
+
+    dispatch(addPost(newPost))
+
+    e.currentTarget.reset()
+  }
+
+  return (
+    <section>
+      <h2>Add a new post</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="postTitle">Post title:</label>
+        <input type="text" id="postTitle" defaultValue="" required />
+
+        <label htmlFor="postContent">Content:</label>
+        <textarea id="postContent" name="postContent" defaultValue="" required />
+
+        <button>Save post</button>
+      </form>
+    </section>
+  )
+}
